b0b15c3bc1a6353aa3021c6b3530f37a426b1e3b
Leon Greiner <leon.greiner@dlr.de>
2025-08-23 19:59:06 +0200

    edited slurm job

--- slurm_job_25.sh
+++ slurm_job_25.sh
@@ -6,7 +6,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time=30:00:00
 #SBATCH --job-name=clutter6d
-#SBATCH --array=1-8
+#SBATCH --array=1-5
 #SBATCH --chdir=/home/grei_lo/Projects/clutter6d
 #SBATCH --output=/home/grei_lo/Projects/clutter6d/job_logs/slurm-%j.out
 #SBATCH --error=/home/grei_lo/Projects/clutter6d/job_logs/slurm-%j.err
@@ -14,14 +14,14 @@
 sleep $(( (SLURM_ARRAY_TASK_ID - 1) * 1 ))
 
 # Set output directory based on array task ID
-OUTPUT_DIR="/volume/hot_storage/slurm_data/grei_lo/clutter6d_${SLURM_ARRAY_TASK_ID}"
+OUTPUT_DIR="/volume/hot_storage/slurm_data/grei_lo/extra_run/clutter6d_${SLURM_ARRAY_TASK_ID}"
 
 # Run the rendering script multiple times to prevent memory issues
-for i in {1..16}; do
+for i in {1..10}; do
     echo "Starting run $i/10 for job ${SLURM_ARRAY_TASK_ID}"
     
     blenderproc run /home/grei_lo/Projects/clutter6d/generate_dataset.py \
-      --config config_slurm.yml \
+      --config config_slurm_25.yml \
       --output_dir "$OUTPUT_DIR"
     
     echo "Completed run $i for job ${SLURM_ARRAY_TASK_ID}"
